553b329c85772f295eacbe6bfadfb609de922ebc
YandreyC <caicedoyeison067@gmail.com>
2024-09-21 19:26:16 -0500

    Ejercicios clase 5 resueltos

--- C5 Fundamentos Javascript/ejercicios/24.js
+++ C5 Fundamentos Javascript/ejercicios/24.js
@@ -4,6 +4,8 @@ function agregarSimboloExclamacion(str) {
   // Por ejemplo: 
   // "hello world" ---> "hello world!"
   // Tu código:
+  let  str1 = str + "!";
+  return str1
 }
 
 module.exports = agregarSimboloExclamacion;
